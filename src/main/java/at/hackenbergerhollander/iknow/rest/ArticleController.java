1643e36519a9d055fbd5ee4d8e020d77229e6132
ReneHollander <rene.hollander@hotmail.de>
2015-12-13 20:43:58 +0100

    added post, put and delete article to rest api

--- src/main/java/at/hackenbergerhollander/iknow/rest/ArticleController.java
+++ src/main/java/at/hackenbergerhollander/iknow/rest/ArticleController.java
@@ -3,10 +3,7 @@ package at.hackenbergerhollander.iknow.rest;
 import at.hackenbergerhollander.iknow.data.article.Article;
 import at.hackenbergerhollander.iknow.data.article.ArticleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,13 +13,31 @@ public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
 
-    @RequestMapping("/articles/{id}")
-    public Article article(@PathVariable("id") int id) {
+    @RequestMapping(value = "/articles", method = RequestMethod.POST)
+    public Article addArticle(@RequestBody Article article) {
+        return articleRepository.save(article);
+    }
+
+    @RequestMapping(value = "/articles/{id}", method = RequestMethod.GET)
+    public Article getArticle(@PathVariable("id") int id) {
         return articleRepository.findById(id);
     }
 
-    @RequestMapping("/articles")
-    public List<Article> article(@RequestParam(value = "search") String searchString) {
+    @RequestMapping(value = "/articles", method = RequestMethod.PUT)
+    public Article putArticle(@RequestBody Article article) {
+        articleRepository.delete(article.getId() + "");
+        return articleRepository.save(article);
+    }
+
+    @RequestMapping(value = "/articles/{id}", method = RequestMethod.DELETE)
+    public Article deleteArticle(@PathVariable("id") int id) {
+        Article article = articleRepository.findById(id);
+        articleRepository.delete(article);
+        return article;
+    }
+
+    @RequestMapping(value = "/articles", method = RequestMethod.GET)
+    public List<Article> searchArticle(@RequestParam(value = "search") String searchString) {
         return articleRepository.search(searchString);
     }
 
